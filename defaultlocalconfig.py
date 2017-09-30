05e7f2857e9aeef63672fef9adde343059eb966c
Joakim Landegren <joakim.landegren@apegroup.com>
2017-10-01 00:17:51 +0200

    Added default local config

---
+++ defaultlocalconfig.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#This file needs to be renamed to localconfig.py after setting the local variables
+
+#IP of bridge
+bridge = "IP_ADDRESS_TO_PHILIPS_HUE_BRIDGE"
+
+#Name of lamp that will change colour
+selectedLamp = "NAME_OF_LAMP_IN_HUE_BRIDGE"
+
+#Dark sky API key
+API_KEY = "DARKSKY_API_KEY"
+
+#GPS position for forecast
+coordinates = ["LATITUDE", "LONGITUDE"]
+
+#Set desired lamp colors [brightness, hue, saturation]
+rainColor = [120, 45000, 254]
+defaultColor = [254, 10000, 0]
