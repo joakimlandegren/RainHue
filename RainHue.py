6f254c7a6e03f64d825a6ade262abe83ee798a2c
Joakim Landegren <joakim.landegren@gmail.com>
2019-11-24 23:17:03 +0100

    Refactored for better testability

--- RainHue.py
+++ RainHue.py
@@ -3,43 +3,56 @@ from phue import Bridge
 import localconfig as cfg
 from forecastiopy import *
 
-# Connect to Philips Hue Bridge
-b = Bridge(cfg.bridge)
-b.connect()
+def connect_to_hue_bridge_and_fetch_lamps():
+    b = Bridge(cfg.bridge)
+    b.connect()
+    global light_names
+    light_names = b.get_light_objects('name')
 
-# Set array with lamp names
-light_names = b.get_light_objects('name')
+def init_lamps(lamp):
+    #Turn lamp on and Set Lamp colors
+    light_names[lamp].on = True
 
-# Get forecast data
-fio = ForecastIO.ForecastIO(cfg.API_KEY, units=ForecastIO.ForecastIO.UNITS_SI, lang=ForecastIO.ForecastIO.LANG_ENGLISH, latitude = cfg.coordinates[0], longitude = cfg.coordinates[1])
-
-# Defines possible weather strings from API to trigger on
-rain_weather_types = ['Rain','Light Rain','Drizzle','Heavy Rain','Possible Light Rain']
-snow_weather_types = ['Snow','Hail','Heavy Snow','Light Snow','Possible Light Snow and Breezy','Possible Light Snow','Light Snow and Breezy']
-
-# Turn lamp on and Set Lamp colors
-lamp = cfg.selectedLamp
-light_names[lamp].on = True
-
-def setLampColor(lampColor):
+def change_lamp_color(lampColor, lamp):
     light_names[lamp].brightness = lampColor[0]
     light_names[lamp].hue = lampColor[1]
     light_names[lamp].saturation = lampColor[2]
 
-def get_weather_conditions_from_darksky():
-    weather = []
+def get_weather():
+    fio = ForecastIO.ForecastIO(cfg.API_KEY, units=ForecastIO.ForecastIO.UNITS_SI,
+                                lang=ForecastIO.ForecastIO.LANG_ENGLISH, latitude=cfg.coordinates[0],
+                                longitude=cfg.coordinates[1])
     if fio.has_hourly() is True:
-        hourly = FIOHourly.FIOHourly(fio)
-        for hour in range(0, 12):
-            if hourly.get_hour(hour)["summary"] in rain_weather_types:
-                weather = ["Rain"]
-                return weather
-            if hourly.get_hour(hour)["summary"] in snow_weather_types:
-                weather = ["Snow"]
-                return weather
-            else:
-                weather.append(hourly.get_hour(hour)["summary"])
-                print(hourly.get_hour(hour)["summary"])
+        hourly_weather = FIOHourly.FIOHourly(fio)
+        return hourly_weather
     else:
         return 'No Hourly data'
-    return weather
+
+
+#Get weather forecast for next 12 hours and set lamp color to blue if it will rain
+def set_weather_color(hourly_weather):
+    # Defines possible weather strings from API to trigger on
+    rainTypes = ['Rain', 'Light Rain', 'Drizzle', 'Heavy Rain']
+    snowTypes = ['Snow', 'Hail', 'Heavy Snow', 'Light Snow']
+
+    for hour in range(0, 12):
+            if hourly_weather.get_hour(hour)["summary"] in rainTypes:
+                color = cfg.rainColor
+                break
+            elif hourly_weather.get_hour(hour)["summary"] in snowTypes:
+                color = cfg.snowColor
+                break
+            else:
+                print(hourly_weather.get_hour(hour)["summary"])
+            color = cfg.defaultColor
+    return color
+
+def main():
+    connect_to_hue_bridge_and_fetch_lamps()
+    lamp = cfg.selectedLamp
+    init_lamps(lamp)
+    weather = get_weather()
+    weather_color = set_weather_color(weather)
+    change_lamp_color(weather_color, lamp)
+
+main()
